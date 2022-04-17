6f25e7e26277a9ddcb37c8bfb7dd9b7f9ca1351a
bogatovam <bogatova.md@gmail.com>
2022-04-17 14:19:40 +0300

    Template repository

---
+++ config/configs_reader.py
@@ -0,0 +1,17 @@
+from omegaconf import OmegaConf
+from os import listdir
+from os.path import isfile, join
+
+configs_files_path = './resources'
+
+all_configs_files_names = [f for f in listdir(configs_files_path) if isfile(join(configs_files_path, f))]
+
+print(f'Found config files: {all_configs_files_names}')
+
+config = OmegaConf.create()
+
+for config_file in all_configs_files_names:
+    file_conf = OmegaConf.load('../source/example3.yaml')
+    config = OmegaConf.merge(config, file_conf)
+
+print(OmegaConf.to_yaml(config))
